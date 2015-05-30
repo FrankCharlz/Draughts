7e64ff5857a7d2cef8b20bbe87ce9c3361380385
laudix <rehemamagoti@gmail.com>
2015-05-30 11:29:58 +0300

    Aslam Aleykum

--- m.js
+++ m.js
@@ -1,3 +1,6 @@
+var kete = ["<img src='img/kete2.png'>", "<img src='img/kete1.png'>"];
+var whose_turn = 0;
+
 
 function makeBoard() {
     var i = 0, j;
@@ -13,58 +16,167 @@ function makeBoard() {
 }
 
 $(document).ready(function(){
-    var images = new Array();
-
-    images[0] = new Image();
-    images[0].src = "img/kete1.png";
-
-    images[1] = new Image();
-    images[1].src = "img/kete2.png";
 
     makeBoard();
-
-    var logbook = $(".logbook");
-
-    $(".row:nth-child(1) > .cell:nth-child(odd)").append(images[1]);
-    $(".row:nth-child(2) > .cell:nth-child(even)").append(images[1]);
-    $(".row:nth-child(3) > .cell:nth-child(odd)").append(images[1]);
-
-    //stupid non selected
-    //$(".row:nth-child(1) > .cell:nth-child(4)").append(images[1]);
-    //$(".row:nth-child(2) > .cell:nth-child(8)").append(images[1]);
-
-    $(".row:nth-child(6) > .cell:nth-child(even)").append(images[0]);
-    $(".row:nth-child(7) > .cell:nth-child(odd)").append(images[0]);
-    $(".row:nth-child(8) > .cell:nth-child(even)").append(images[0]);
+    fillKete();
 
     var carrying = false;
     var from;
+    var carried_kete_type;
 
     $(".cell").click(function() {
         var id =  $(this).attr('id');
-        var kete_str =  $("#"+id+" > img").attr('src')+"";
-        var kete_type = (kete_str.contains('2')) ? 0 : 1;
+        var kete_type = getKeteType(id);
+
+        log("clicked : "+id+" kete_type : "+kete_type);
 
 
         if (!carrying) {
-            carrying = true;
-            from = id;
+            if(kete_type != -1 && kete_type == whose_turn) {
+                //if box not empty pick the kete
+                carried_kete_type = kete_type;
+                from = id;
+                carrying = true;
+                log("Move started: PLAYER["+carried_kete_type+"]");
+            }
+            if (kete_type != whose_turn) {
+                alert("Not your turn bitch..");
+            }
         }
-        else {
-            $("#"+from).empty();
-            $("#"+id).append(images[kete_type]);
-            carrying = false;
+        else
+        {
+            //if carrying..
+            if (canDropHere(from, id, carried_kete_type, kete_type)) {
+                //if box empty drop the kete and is allowed to drop
+                removeKete(from);
+                putKete(id, carried_kete_type)
+                carrying = false;
+                log("Move completed:")
+                whose_turn = (whose_turn+1)%2;
+            } else {
+                alert("You can not drop here fucker...");
+            }
+
         }
-        
-        logbook.text("Clicked : "+id);
 
     });
 
 
-
 });
 
+function canDropHere(from, to, ktype, destination_ktype) {
+    //dis  depends on the name celc_r_c
+    //if u change it u r fucked
+    //return true if r+c is odd..
+    var sum_r_c = parseInt(to.charAt(5)) + parseInt(to.charAt(7));
+    if (sum_r_c & 1 == 1) {return false;}
+
+    //limiting back and forward motions
+    var from_row = parseInt(from.charAt(5));
+    var to_row = parseInt(to.charAt(5));
+    var from_cell = parseInt(from.charAt(7));
+    var to_cell = parseInt(to.charAt(7));
+
+    if (from_cell==to_cell && from_row==to_row && ktype==destination_ktype){
+        //rudisha kete ucheze tena
+        removeKete(from);//to allow adding it again...
+        whose_turn = (whose_turn+1)%2;//to stop the turn
+        return true;
+    }
+
+    if (destination_ktype != -1){
+        //if destination is not empty
+        return false;
+    }
+
+    if (Math.abs(to_row - from_row) == 1) {
+        //simple jump forward..
+        if (ktype == 0  && from_row < to_row) {return true;}
+        if (ktype == 1  && from_row > to_row) { return true; }
+    }
+
+    if (Math.abs(to_row - from_row) == 2) {
+        //move ya kula..
+        log("MOVE YA KULA");
+
+        //intermediate coords
+        var i_r = parseInt((from_row + to_row)/2);
+        var i_c = parseInt((from_cell + to_cell)/2);
+        var i_id = "cell_"+i_r+"_"+i_c;
+
+        //intermediate kete id....
+        var it = getKeteType(i_id);
+        if(it != -1 && it != ktype) {
+            log("Your good .. Eat..!");
+            removeKete(i_id);
+            return true;
+        }
+    }
+
+    return false;
+}
+
+function hasKete(id) {
+    return (document.getElementById(id).innerHTML.lastIndexOf("<i", 0) == 0);
+}
+
+function getKeteType(id){
+    //returns -1 if box empty or 0|1 for other cases
+    if (!hasKete(id)) { return -1; }
+    if (document.getElementById(id).innerHTML.contains('1')){
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+function putKete(id, keteType) {
+    document.getElementById(id).innerHTML = kete[keteType];
+}
+
+function removeKete(id) {
+    document.getElementById(id).innerHTML = "";
+}
+
+function log(str) {
+    $(".logbook").append("<br>"+str);
+}
+
+function fillKete() {
+    var r=0;
+    var c=0;
+
+    for (r=0; r<3; r++){
+        for(c=r%2; c<8; c+=2) {
+            var cid = "cell_"+r+"_"+c;
+            putKete(cid, 0);
+        }
+    }
+
+    for (r=5; r<8; r++){
+        for(c=r%2; c<8; c+=2) {
+            var cid = "cell_"+r+"_"+c;
+            putKete(cid,1);
+        }
+    }
+}
 
 String.prototype.contains = function(it) { return this.indexOf(it) != -1; };
 
-
+var Point = function (id) {
+    this.row = parseInt(id.charAt(5));
+    this.cell= parseInt(id.charAt(7));
+    this.isValid = function() {
+        return ( (this.row + this.cell) & 1 == 0) ? true : false;
+    }
+    this.keteType = function() {
+        return getKeteType(id);
+    }
+    this.getId = id;
+    this.between = function(anotherId){
+        var p = new Point(anotherId);
+        var r = parseInt((this.row + p.row)/2);
+        var c = parseInt((this.cell + p.cell)/2);
+        return new Point("cell_"+r+"_"+c);
+    }
+}
